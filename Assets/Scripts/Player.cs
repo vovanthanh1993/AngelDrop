b9ece24b238dd5193978bf13f93fb8c817799599
vovanthanh1993 <vothanh199311t1@gmail.com>
2024-11-06 00:18:15 +0700

    Change controls on mobile devices

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Pot : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float moveSpeed = 1;
     float xDirection;
-
-    private bool isDragging = false;
-    private Vector3 offset;
     private Camera mainCamera;
     float startY;
 
@@ -23,6 +20,8 @@ public class Pot : MonoBehaviour
     void Update()
     {
         if (GameController.Ins.isGameOver()) return;
+
+        // Dieu khien bang ban phim
         xDirection = Input.GetAxisRaw("Horizontal");
         float moveStep = moveSpeed * Time.deltaTime * xDirection;
 
@@ -31,40 +30,19 @@ public class Pot : MonoBehaviour
         // Lấy vị trí của góc trên bên phải
         Vector3 topRight = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width - 20, Screen.height, 0));
 
+        // Gioi han khi di chuyen bang ban phim
         if (transform.position.x <= topLeft.x && xDirection == -1 || transform.position.x >= topRight.x && xDirection == 1) return;
         transform.position += new Vector3(moveStep, 0, 0);
 
-        // Kiểm tra nếu có touch trên màn hình
+        // Di chuyen tren thiet bi di dong
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
+            // Chuyển đổi vị trí chạm trên màn hình thành tọa độ thế giới
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
 
-            // Bắt đầu kéo khi ngón tay chạm vào Sprite
-            if (touch.phase == TouchPhase.Began)
-            {
-                Vector3 touchPosition = mainCamera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0));
-                RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector2.zero);
-
-                if (hit.collider != null && hit.collider.gameObject == gameObject)
-                {
-                    isDragging = true;
-                    offset = transform.position - touchPosition;
-                }
-            }
-            // Di chuyển Sprite khi kéo ngón tay
-            else if (touch.phase == TouchPhase.Moved && isDragging)
-            {
-                // Chuyển đổi vị trí chạm trên màn hình thành tọa độ thế giới
-                Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-
-                // Giữ nguyên vị trí Y, chỉ thay đổi vị trí X theo vị trí chạm
-                transform.position = new Vector3(touchPosition.x, startY, 5);
-            }
-            // Dừng kéo khi ngón tay rời màn hình
-            else if (touch.phase == TouchPhase.Ended)
-            {
-                isDragging = false;
-            }
+            // Giữ nguyên vị trí Y, chỉ thay đổi vị trí X theo vị trí chạm
+            transform.position = new Vector3(touchPosition.x, startY, 5);
         }
     }
 }
